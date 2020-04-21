e1d8e933831c1c12d457bc14b1e0b59076e2675c
MFRasfan <rasfanfaiz@gmail.com>
2020-04-21 15:34:55 -0400

    userdone

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const mongoose = require ('mongoose')
 const morgan = require ('morgan')
 const bodyParser = require ('body-parser')
 const cookieParser = require ('cookie-parser')
-const expressValidator = require ('express-validator')
+const expressValidator = require('express-validator');
 
 
 // load env variables
 require ('dotenv').config()
 // import routes
+const authRoutes = require('./routes/auth')
 const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
+
+
 
 // app
 const app = express ()
@@ -33,7 +37,11 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
 // routes middleware
+app.use("/api", authRoutes);
 app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+
+
 
 const port = process.env.PORT || 8000
 
--- routes/user.js
+++ routes/user.js
@@ -2,11 +2,19 @@
 const express = require ('express')
 const router = express.Router()
 
+const {requireSignin, isAuth, isAdmin } = require ("../controllers/auth")
+const { userById} = require('../controllers/user');
 
-const { signup } = require ("../controllers/user")
-const { userSignupValidator } = require ("../validator")
+
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin,(req, res) => {
+    res.json({
+        user: req.profile
+    })
+
+});
+
+router.param('userId', userById);
 
 
-router.post("/signup", userSignupValidator, signup);
 
 module.exports = router;
