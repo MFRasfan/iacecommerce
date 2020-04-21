75b0998c65c158e60807c47c02419759e8911574
MFRasfan <rasfanfaiz@gmail.com>
2020-04-21 17:26:31 -0400

    cata

--- controllers/category.js
+++ controllers/category.js
@@ -3,15 +3,14 @@ const {errorHandler} = require('../helpers/dbErrorHandler')
 
 
 
-exports.create = (req, res) =>{
-    const category = new Category(req.body)
-    category.save((err, data) =>{
-        if (error){
+exports.create = (req, res) => {
+    const category = new Category(req.body);
+    category.save((err, data) => {
+        if (err) {
             return res.status(400).json({
                 error: errorHandler(err)
             });
         }
         res.json({ data });
-    })
-}
-
+    });
+};
--- routes/category.js
+++ routes/category.js
@@ -1,14 +1,16 @@
-// import express
-const express = require ('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-
-const { create } = require ("../controllers/category")
-
-
-router.post("/category/create", create);
+const { create } = require('../controllers/category');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+const { userById } = require('../controllers/user');
 
 
 
+router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+
+
+
+router.param('userId', userById);
 
 module.exports = router;
